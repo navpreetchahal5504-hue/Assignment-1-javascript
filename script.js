cf561b68f4241a4490cef484ba6296f022a5a8a4
Navpreet Kaur <navpreetchahal5504@gmail.com>
2026-06-07 22:44:25 -0400

    Added index variables for Mission selections

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const locations =["Mars Base", "Tokyo Sector", "Arctic Section", "Mpon Colony"];
 const weapons =["Laser Refile", "Plasma Cannon", "EMP Device", "Nano Blade"];
 const objectives =["Rescue Scientists", "Hack Mainframe", "Recover Artifact","Destroy Drone Factory"];
 const agents =["Low", "Medium", "High", "Extreme"];
+
+let agentsIndex = -1;
+let locationIndex = -1;
+let weaponIndex = -1;
+let objectiveIndex = -1;
+let riskIndex = -1;
