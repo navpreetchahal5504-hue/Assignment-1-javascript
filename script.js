dd4400b834e70a30002741c42626450c998ea75b
Navpreet Kaur <navpreetchahal5504@gmail.com>
2026-06-07 22:58:33 -0400

    Implemented cycling functionality for missions options

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ let locationIndex = -1;
 let weaponIndex = -1;
 let objectiveIndex = -1;
 let riskIndex = -1;
+
+function cycleOption(array, index){
+    return (index + 1) % array.lenth;
+}
+
+document.getElementById("agentBtn").addEventListener("click"), function(){
+    agentIndex = cycleOption(agents, agentIndex);
+    document.getElementById("agentDisplay").textContent = agents[agentIndex];
+}
+
+document.getElementById("locationBtn").addEventListener("click"), function(){
+    locationIndex = cycleOption(locations, locationIndex);
+    document.getElementById("locationDisplay").textContent = locations[locationIndex];
+}
+
+document.getElementById("weaponBtn").addEventListener("click"), function(){
+    weaponIndex = cycleOption(weapons, weaponIndex);
+    document.getElementById("weaponDisplay").textContent = weapons[weaponIndex];
+}
+
+document.getElementById("objectiveBtn").addEventListener("click"), function(){
+    objectiveIndex = cycleOption(objectives, objectiveIndex);
+    document.getElementById("objectiveDisplay").textContent = objectives[objectiveIndex];
+}
+
+document.getElementById("riskBtn").addEventListener("click"), function(){
+    riskIndex = cycleOption(risks, riskIndex);
+    document.getElementById("riskDisplay").textContent = risks[riskIndex];
+}
+
