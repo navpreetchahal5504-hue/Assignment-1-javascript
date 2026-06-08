728f73a6708ded9ac94673d8487a7ab2da6b0bec
Navpreet Kaur <navpreetchahal5504@gmail.com>
2026-06-07 22:40:23 -0400

    Initialize game data arrays in script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const agents =["Ghost", "Shadow", "Viper", "Nova"];
+const locations =["Mars Base", "Tokyo Sector", "Arctic Section", "Mpon Colony"];
+const weapons =["Laser Refile", "Plasma Cannon", "EMP Device", "Nano Blade"];
+const objectives =["Rescue Scientists", "Hack Mainframe", "Recover Artifact","Destroy Drone Factory"];
+const agents =["Low", "Medium", "High", "Extreme"];
